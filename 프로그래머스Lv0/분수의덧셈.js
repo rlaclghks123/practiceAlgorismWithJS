7419f89201f091d3815cd4b62e5102e95252f273
CHI HWAN KIM <chiman@CHIui-MacBookPro.local>
2022-12-12 16:46:50 +0900

    분수의 덧셈 설명 추가

--- 프로그래머스Lv0/분수의덧셈.js
+++ 프로그래머스Lv0/분수의덧셈.js
@@ -1,3 +1,4 @@
+// 최대공약수(gcd)를 구하는 함수
 function gcd(a, b) {
   if (a % b === 0) return b;
   return gcd(b, a % b);
@@ -6,10 +7,15 @@ function gcd(a, b) {
 function solution(denum1, num1, denum2, num2) {
   var answer = [];
 
+  // 1. 두 분수의 합을 구한다.
+
   const boonJa = denum1 * num2 + denum2 * num1;
   const boonMo = num1 * num2;
 
+  // 2. 최대 공약수를 구한다.
   const gcd_val = gcd(boonJa, boonMo);
+
+  // 3. answer 배열에 분수의 합을 기약분수 형태로 나타낸다.(최대공약수로 나눈다)
   answer = [boonJa / gcd_val, boonMo / gcd_val];
 
   return answer;
