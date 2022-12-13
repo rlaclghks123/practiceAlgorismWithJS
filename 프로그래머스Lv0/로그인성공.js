410598bd91acd4f455a68c6811ba701edfbdd66f
chiman <rlaclghks123@naver.com>
2022-12-13 22:19:27 +0900

    github email 변경

--- 프로그래머스Lv0/로그인성공.js
+++ 프로그래머스Lv0/로그인성공.js
@@ -10,7 +10,7 @@ function solution(id_pw, db) {
     // 2. id는 db에 있지만 pw값이 db에 존재하지 않는 경우
     if (list.includes(id)) return (answer = 'wrong pw');
 
-    // 3. id, pw정보가 db에 없는 경우
+    // 3. id, pw정보가 db에 없는 경우.
     if (!list.includes(id) && !list.includes(pw)) return (answer = 'fail');
   });
   return answer;
