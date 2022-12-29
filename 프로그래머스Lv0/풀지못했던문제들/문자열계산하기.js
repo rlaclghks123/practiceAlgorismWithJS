7afd0ad47835b62ddd339374a5c63b61a71279d1
chiman <rlaclghks123@naver.com>
2022-12-29 19:05:57 +0900

    문자열 계산하기 완료

---
+++ 프로그래머스Lv0/풀지못했던문제들/문자열계산하기.js
@@ -0,0 +1,27 @@
+// my_string의 문자열의 수식을 계산한 값을 return
+// 연산자는 +, - 만 존재한다.
+
+function solution(my_string) {
+  // 1. 문자열을 " "을 기준으로 나눠준다.
+  const splited = my_string.split(' ');
+  let answer = Number(splited[0]);
+
+  // 2. splited배열을 돌면서 +인경우 더하고, -인경우 빼준다. 단 0번째 값은 초기값으로 넣어주고, 1번째부터 계산을 해준다.
+  splited.forEach((item, index) => {
+    if (item === '+') {
+      answer += Number(splited[index + 1]);
+    } else if (item === '-') {
+      answer -= Number(splited[index + 1]);
+    }
+  });
+
+  return answer;
+}
+
+solution('3 + 4'); //7
+
+solution('5 - 3'); // 2
+
+solution('5 - 3 + 2'); // 4
+
+// 2번통과
