f76c33b03b9d3f126c77a04d05a9bc5d7b864c1d
chiman <rlaclghks123@naver.com>
2023-01-09 12:26:30 +0900

    자료구조 코드 수정

--- 자료구조/DoublyLinkedList.js
+++ 자료구조/DoublyLinkedList.js
@@ -1,151 +1,186 @@
 class Node {
   constructor(data) {
     this.data = data;
-    this.prev = null;
     this.next = null;
+    this.prev = null;
   }
 }
 
 class DoublyLinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
+    this.head = null; //처음에 데이터가 없다면 head는 null이다.
+    this.tail = this.head;
+    this.size = 0; //리스트의 크기를 나타내며, 초기값은 0이다.
   }
 
   isEmpty() {
     return this.size == 0;
   }
 
-  addHead(value) {
-    //if list is empty
+  insertHead(data) {
     if (this.isEmpty()) {
-      this.head = new Node(value);
-      this.tail = this.head;
+      this.head = new Node(data);
+      this.size++;
     } else {
-      let newNode = new Node(value);
-      newNode.next = this.head;
-      this.head.prev = newNode;
+      let current = this.head;
+      let newNode = new Node(data);
       this.head = newNode;
+      newNode.next = current;
+      current.prev = newNode;
+      this.size++;
     }
-    this.size++;
   }
 
-  addTail(value) {
-    //if list is empty
+  insertTail(data) {
+    let newNode = new Node(data);
+
     if (this.isEmpty()) {
-      this.tail = new Node(value);
-      this.head = this.tail;
+      this.head = new Node(data);
+      this.size++;
     } else {
-      let newNode = new Node(value);
-      newNode.prev = this.tail;
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
-
-  removeByValue(value) {
-    let current = this.head; //Using tail instead of head also works.
-
-    //if head is target
-    if (current.data == value) {
-      this.head = current.next;
-      this.size--;
-    } else {
-      let prev = current;
+      let current = this.head;
       while (current.next) {
-        if (current.data == value) {
-          prev.next = current.next;
-          current = current.next;
-          break;
-        }
-        prev = current;
         current = current.next;
       }
 
-      if (current.data == value) prev.next = null;
-      this.size--;
+      current.next = newNode; // next가 null === tail 이므로 newNode를 넣어준다.
+      newNode.prev = current;
     }
+    this.size++; //length 는 1증가
   }
 
-  removeHead() {
-    if (this.isEmpty()) return null;
+  insert(data, index) {
+    if (index > 0 && index > this.size) {
+      console.log('범위를 벗어났습니다');
+      return;
+    }
+    // 범위를 벗어나면 종료해준다.
+    if (index === 0) {
+      let current = this.head;
+      let newNode = new Node(data);
+      this.head = newNode;
+      newNode.next = current;
+      current.prev = newNode;
+      this.size++;
+      return;
+    }
 
-    let data = this.head.data;
+    let currentNode = this.head;
+    let newNode = new Node(data);
+    let count = 0;
+    let temp;
 
-    //if head(=tail) is target
-    if (this.head == this.tail) {
-      this.head = null;
-      this.tail = null;
+    while (count < index - 1) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    temp = currentNode.next;
+    currentNode.next = newNode;
+    newNode.next = temp;
+    newNode.prev = currentNode;
+
+    this.size++;
+  }
+
+  getAt(index) {
+    let currentNode = this.head;
+    let count = 0;
+
+    if (currentNode === null) return null;
+    while (currentNode) {
+      if (count == index) {
+        console.log(currentNode.data);
+      }
+      count++;
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  remove(index) {
+    if (index > 0 && index > this.size) {
+      console.log('범위를 벗어났습니다');
+      return;
+    }
+
+    let currentNode = this.head;
+
+    let count = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = currentNode.next;
     } else {
-      this.head = this.head.next;
-      this.head.prev = null;
+      //loop를 통해 해당 index의 연결고리를 끊는다.
+      while (count < index) {
+        count++;
+        currentNode = currentNode.next;
+      }
+
+      let temp;
+
+      temp = currentNode.next;
+      currentNode.prev.next = temp;
     }
+
     this.size--;
-    return data;
   }
 
-  removeTail() {
-    if (this.isEmpty()) return null;
-
-    let data = this.tail.data;
-
-    //if head(=tail) is target
-    if (this.head == this.tail) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.tail = this.tail.prev;
-      this.tail.next = null;
-    }
-    this.size--;
-    return data;
+  clear() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  findStartingHead(value) {
-    let current = this.head;
-    while (current.next) {
-      if (current.data == value) return true;
-      current = current.next;
-    }
-    return false;
-  }
-
-  findStartingTail(value) {
-    let current = this.tail;
-    while (current.prev) {
-      if (current.data == value) return true;
-      current = current.prev;
-    }
-    return false;
-  }
   print() {
     let result = '';
-    let current = this.head;
-    if (current === null) return null;
-    while (current.next) {
-      result += `${current.data} -> `;
-      current = current.next;
+    let currentNode = this.head;
+
+    if (currentNode === null) return null;
+    while (currentNode.next) {
+      result += `${currentNode.data} `;
+      currentNode = currentNode.next;
     }
-    result += current.data;
+    result += currentNode.data;
+
     console.log(result);
   }
 }
+const linkedList = new DoublyLinkedList();
 
-let doubleLinkedList = new DoublyLinkedList();
+// isEmpty Test
 
-for (let i = 1; i <= 5; i++) doubleLinkedList.addHead(i);
-doubleLinkedList.print(); //5->4->3->2->1
+console.log(linkedList.isEmpty()); // true 출력
 
-doubleLinkedList.removeHead();
-doubleLinkedList.print(); //4->3->2->1
+// insertHead Test
 
-doubleLinkedList.removeByValue(3);
+linkedList.insertHead(100);
+linkedList.print(); // 100 출력
 
-doubleLinkedList.print(); //4->2->1
-doubleLinkedList.removeTail();
-doubleLinkedList.print(); //4->2
+linkedList.insertHead(200);
+linkedList.print(); // 200 100 출력
 
-doubleLinkedList.addTail(1);
-doubleLinkedList.print(); //4->2->1
+linkedList.insertHead(300);
+linkedList.print(); // 300 200 100 출력
+console.log(linkedList.isEmpty()); // false 출력
+
+// insertTail Test
+linkedList.insertTail(400);
+linkedList.print(); // 300 200 100 400 출력
+
+linkedList.insert(500, 2);
+linkedList.insert(900, 0);
+linkedList.print(); // 900 300 200 500 100 400 출력
+
+linkedList.getAt(3); // 500 출력
+
+linkedList.remove(0);
+linkedList.print(); // 300 200 500 100 400 출력
+
+linkedList.remove(2);
+linkedList.print(); // 300 200 100 400 출력
+
+linkedList.clear();
+linkedList.insertHead(100);
+linkedList.print(); // 100 출력
--- 자료구조/SingleLinkedList.js
+++ 자료구조/SingleLinkedList.js
@@ -1,112 +1,173 @@
 class Node {
-  constructor(item) {
-    this.data = item;
+  constructor(data) {
+    this.data = data;
     this.next = null;
   }
 }
 
 class SingleLinkedList {
   constructor() {
-    this.head = null;
-    this.size = 0;
+    this.head = null; //처음에 데이터가 없다면 head는 null이다.
+    this.tail = null;
+    this.size = 0; //리스트의 크기를 나타내며, 초기값은 0이다.
   }
 
   isEmpty() {
     return this.size == 0;
   }
 
-  add(item) {
+  insertHead(data) {
     if (this.isEmpty()) {
-      this.head = new Node(item);
+      this.head = new Node(data);
       this.size++;
     } else {
-      let newNode = this.head;
-      this.head = new Node(item);
-      this.head.next = newNode;
+      let current = this.head;
+      this.head = new Node(data);
+      this.head.next = current;
       this.size++;
     }
   }
 
-  removeByValue(item) {
-    let current = this.head;
-
-    if (current.data == item) {
-      this.head = current.next;
-      this.size--;
+  insertTail(data) {
+    let newNode = new Node(data);
+    if (this.isEmpty()) {
+      this.head = new Node(data);
+      this.size++;
     } else {
-      let prev = current;
-      let error = true;
+      let current = this.head;
 
       while (current.next) {
-        if (current.data == item) {
-          prev.next = current.next;
-          current = current.next;
-          error = false;
-          break;
-        }
-        prev = current;
+        // tail을 찾아준다.
         current = current.next;
       }
-      if (current.data == item) {
-        prev.next = null;
-        error = false;
+
+      current.next = newNode; // next가 null === tail 이므로 newNode를 넣어준다.
+    }
+    this.size++; //length 는 1증가
+  }
+
+  insert(data, index) {
+    if (index > 0 && index > this.size) {
+      console.log('범위를 벗어났습니다');
+      return;
+    }
+    // 범위를 벗어나면 종료해준다.
+    if (index === 0) {
+      let current = this.head;
+      this.head = new Node(data);
+      this.head.next = current;
+      this.size++;
+      return;
+    }
+
+    let currentNode = this.head;
+    let newNode = new Node(data);
+    let count = 0;
+    let temp;
+
+    while (count < index - 1) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    temp = currentNode.next;
+    currentNode.next = newNode;
+    newNode.next = temp;
+
+    this.size++;
+  }
+
+  getAt(index) {
+    let currentNode = this.head;
+    let count = 0;
+
+    if (currentNode === null) return null;
+    while (currentNode) {
+      if (count == index) {
+        console.log(currentNode.data);
+      }
+      count++;
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  remove(index) {
+    if (index > 0 && index > this.size) {
+      console.log('범위를 벗어났습니다');
+      return;
+    }
+
+    let currentNode = this.head;
+
+    let count = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = currentNode.next;
+    } else {
+      //loop를 통해 해당 index의 연결고리를 끊는다.
+      while (count < index - 1) {
+        count++;
+        currentNode = currentNode.next;
       }
 
-      if (error) console.log(`There is no ${item}`);
-      else this.size--;
+      currentNode.next = currentNode.next.next;
     }
+
+    this.size--;
   }
 
-  removeHead() {
-    let item = null;
-    if (this.head != null) {
-      item = this.head.data;
-      this.head = this.head.next;
-      this.size--;
-    }
-    return item;
-  }
-
-  search(value) {
-    let current = this.head;
-    if (current === null) return null;
-    while (current.next) {
-      if (current.data == value) return true;
-      current = current.next;
-    }
-    return false;
+  clear() {
+    this.head = null;
+    this.size = 0;
   }
 
   print() {
     let result = '';
-    let current = this.head;
+    let currentNode = this.head;
 
-    while (current.next) {
-      result += `${current.data} `;
-      current = current.next;
+    if (currentNode === null) return null;
+    while (currentNode.next) {
+      result += `${currentNode.data} `;
+      currentNode = currentNode.next;
     }
-    result += current.data;
+    result += currentNode.data;
 
     console.log(result);
   }
 }
+const linkedList = new SingleLinkedList();
 
-let singleLinkedList = new SingleLinkedList();
+// isEmpty Test
 
-console.log(singleLinkedList.isEmpty()); // true 출력
-singleLinkedList.add(1); // 1 추가
+console.log(linkedList.isEmpty()); // true 출력
 
-singleLinkedList.print(); // 1 출력
-console.log(singleLinkedList.isEmpty()); // false 출력
+// insertHead Test
 
-singleLinkedList.add(2); // 2 추가
-singleLinkedList.print(); // 2 1 출력
-singleLinkedList.removeHead(); // head인 2 제거
+linkedList.insertHead(100);
+linkedList.print(); // 100 출력
 
-singleLinkedList.print(); // 1 출력
-singleLinkedList.add(2); // 2 추가
-singleLinkedList.add(3); // 3 추가
-singleLinkedList.removeByValue(2);
-singleLinkedList.print(); // 3 1 출력
+linkedList.insertHead(200);
+linkedList.print(); // 200 100 출력
 
-singleLinkedList.removeByValue(2); // There is no 2 출력
+linkedList.insertHead(300);
+linkedList.print(); // 300 200 100 출력
+console.log(linkedList.isEmpty()); // false 출력
+
+// insertTail Test
+linkedList.insertTail(400);
+linkedList.print(); // 300 200 100 400 출력
+
+linkedList.insert(500, 2);
+linkedList.insert(900, 4);
+linkedList.print(); // 300 200 500 100 400 출력
+
+linkedList.getAt(3); // 100 출력
+
+linkedList.remove(1);
+linkedList.print(); // 300 200 500 400 출력
+
+linkedList.clear();
+linkedList.insertHead(100);
+linkedList.print(); // 100 출력
