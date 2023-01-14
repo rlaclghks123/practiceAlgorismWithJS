d5f987f38e5457b9eb4dcd9502287e4da6de9124
chiman <rlaclghks123@naver.com>
2023-01-14 12:10:07 +0900

    가장 가까운 같은 글자 완료

---
+++ 프로그래머스Lv1/못푼문제/가장가까운같은글자.js
@@ -0,0 +1,56 @@
+// s의 각 위치마다 자신보다 앞에 나왔으면서 자신과 가장 가까운곳에 있는 글자를 찾기
+
+// function solution(s) {
+//   var answer = [];
+
+//   // 1. 문자열을 문자 하나씩 가져와 stack에 없으면 stack에 담고 answer에 -1을, stack에 있으면 answer에 index값을 담는다..
+//   let stack = [];
+//   [...s].forEach((item, i) => {
+//     if (!stack.includes(item)) {
+//       answer.push(-1);
+//     } else {
+//       let index = i;
+//       let count = 0;
+//       while (item !== stack[index]) {
+//         count++;
+//         index--;
+//         if (item === stack[index]) {
+//           answer.push(count);
+//           break;
+//         }
+//       }
+//     }
+//     stack.push(item);
+//   });
+
+//   return answer;
+// }
+
+// 다른사람 코드
+// slice(0,i)를 통해 마지막값을 제외하고 나머지를 잘라서 lastIndex값을 찾아, 현재값의 i에서 count의 차이를 구해주며, 음수일 경우 -1로 지정해준다.
+
+// const solution = (s) =>
+//   [...s].map((char, i) => {
+//     const count = s.slice(0, i).lastIndexOf(char);
+//     return count < 0 ? count : i - count;
+//   });
+
+// solution('banana'); //	[-1, -1, -1, 2, 2, 2]
+
+// solution('foobar'); //	[-1, -1, 1, -1, -1, -1]
+
+function solution(s) {
+  var answer = [];
+
+  answer = [...s].map((item, i) => {
+    const count = s.slice(0, i).lastIndexOf(item);
+    console.log(count);
+    return count < 0 ? -1 : i - count;
+  });
+
+  return answer;
+}
+
+solution('banana'); //	[-1, -1, -1, 2, 2, 2]
+
+solution('foobar'); //	[-1, -1, 1, -1, -1, -1]
