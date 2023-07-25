536bd34c1a644f823fcdd69783ec37d3d827f020
chiman <rlaclghks123@naver.com>
2023-07-25 11:45:45 +0900

    프로그래머스 Lv2 n진수 게임 복습 완료

--- 프로그래머스Lv2/[3차]n진수게임.js
+++ 프로그래머스Lv2/[3차]n진수게임.js
@@ -25,6 +25,29 @@ function solution(n, t, m, p) {
   return answer.slice(0, t).toUpperCase();
 }
 
+// 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p 가 주어진다
+
+// 1. 0부터 튜브가 말해야 하는 숫자 t * 참가인원 만큼의 숫자를 n진법으로 바꿔서 tmp에 담아줍니다.
+// 2. m명중 p번째 값을 t인 경우만 담은 새로운 배열 map을 통해 만들어줍니다.
+// 3. 2에서 만든 배열을 문자열로 바꿔줍니다.
+// 4. 3에서 만든 문자열을 t의 길이만큼 잘라준 뒤, 대문자로 바꿔 출력합니다.
+
+function solution(n, t, m, p) {
+  let tmp = '';
+
+  for (let i = 0; i <= t * m; i++) {
+    tmp += i.toString(n);
+  }
+
+  return [...tmp]
+    .map((item, idx) => {
+      if (idx % m === p - 1) return item;
+    })
+    .join('')
+    .slice(0, t)
+    .toUpperCase();
+}
+
 solution(2, 4, 2, 1); // "0111"
 solution(16, 16, 2, 1); // "02468ACE11111111"
 solution(16, 16, 2, 2); // "13579BDF01234567"
