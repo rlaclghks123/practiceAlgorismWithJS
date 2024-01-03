0d21f2b6c3a6057a03b8b2b23673b902ecdd09b4
rlaclghks123 <rlaclghks123@naver.com>
2024-01-03 18:06:23 +0900

    Lv1 프로그래머스 2문제 풀기
    덧칠하기, 실패율문제 풀기

--- 프로그래머스Lv1/덧칠하기.js
+++ 프로그래머스Lv1/덧칠하기.js
@@ -1,30 +1,34 @@
-// 구역 n, 왼쪽부터 1번부터 n번
-// 페인트 칠하는 롤러의 길이는 m미터
-// 롤러가 벽에 벗어나면 안됨
-// 구역의 일부분만 포함되도록 칠하면 안됨
+//  페인트가 칠해진 길이가 n미터인 벽, 벽에 페인트를 칠하는 롤러의 길이는 m미터이고, 롤러로 벽에 페인트를 한 번 칠하는 규칙은 다음과 같습니다.
+// 롤러가 벽에서 벗어나면 안 됩니다.
+// 구역의 일부분만 포함되도록 칠하면 안 됩니다.
+// 다시 페인트를 칠하기로 정한 구역들의 번호가 담긴 정수 배열 section
+// 롤러로 페인트칠해야 하는 최소 횟수를 return 하는 solution 함수를 작성해 주세요.
+
+// 1. n크기의 모두 true로 가득찬 배열을 하나 만든다.
+// 2. section을 돌면서 적혀있는 index값을 1에서 만든 배열의 index-1값에 false로 만들어준다. -> section은 1부터 시작하기 때문
+// 3. 1,2에서 수정한 배열을 순회하면서 false이면서 범위를 벗어나지 않는경우 룰러로 페인트를 칠한다.
+// 3-1. for문을 통해 i번째에 룰러의 길이인 m만큼 반복하여 모두 true로 만들어준다.
+// 3-2. count+1을 해준다.
+// 4. 만약 다 칠해지지 않은 곳이 하나라도 있다면 +1을 더해준다.
+// 5. 최종적으로 count한 값을 출력한다.
 
 function solution(n, m, section) {
-  // 기본적으로 1로 초기화 해줍니다.
-  let wall = Array.from({ length: n }, () => 1);
+  const isPaintArr = Array.from({ length: n }, () => true);
+  let answer = 0;
 
-  // section을 돌면서 다시 칠해야 하는 부분을 0으로 바꿔줍니다. -1을 해준 이유는 wall은 0번째 부터 시작하기 때문
-  section.forEach((item) => {
-    wall[item - 1] = 0;
-  });
+  section.forEach((item) => (isPaintArr[item - 1] = false));
 
-  let cnt = 0;
-
-  // wall을 돌면서 item이 0이면서, i+m이 n보다 작거나 같은경우에 for문을 돌아 i부터 i+m까지 1로 바꿔줍니다.(페인트칠)
-  // 칠해주고 난 뒤, cnt를 해줍니다.
-  wall.forEach((item, i) => {
-    if (item === 0 && i + m <= n) {
+  isPaintArr.forEach((item, i) => {
+    if (!item && i + m <= n) {
       for (let j = i; j < i + m; j++) {
-        wall[j] = 1;
+        isPaintArr[j] = true;
       }
-      cnt++;
+      answer += 1;
     }
   });
-  // wall에 0이 남아있다면 1번 더 페인트칠 해야하기 때문에 cnt를 해주고, 총 cnt값을 출력합니다.
-  if (wall.includes(0)) cnt++;
-  return cnt;
+
+  const isDone = isPaintArr.every((item) => item);
+  if (!isDone) answer += 1;
+
+  return answer;
 }
--- 프로그래머스Lv1/못푼문제/또풀지못한문제들/실패율.js
+++ 프로그래머스Lv1/못푼문제/또풀지못한문제들/실패율.js
@@ -1,65 +1,20 @@
-// 실패율은 스테이지에 도달했으나 아직 클리어하지 못한 플레이어수 / 스테이지에 도달한 플레이어수
-// 전체스테이지의 개수 n,  현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
-// 실패율이 높은 스테이지부터 내림차순으로 스테이지 번호가 담긴 배열을 return
-// 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저오도록
-// 스테이지에 도달한 유저가 없는 경우 실패율은 0
+// 1. 실패율을 구한다. -> 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+// 1-1. 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수를 구한다.
+// 1-2. 스테이지에 도달한 플레이어 수를 구한다.
+// 2. 실패율, idx가 담긴 배열을 만든다.
+// 3. 2에서 만든 배열을 내림차순으로 만들고, 만약 실패율이 같다면 낮은 순서로 담아 출력한다.
 
 function solution(N, stages) {
-  let result = [];
-  for (let i = 1; i <= N; i++) {
-    // 1. 1부터 n까지 반복하여 stage의 요소 x가 i보다 크거나 같은경우(스테이지에 도달한 플레이어)의 수를 구해준다.
-    let reach = stages.filter((x) => x >= i).length;
-
-    // 2. stage의 요소 x와 같은 경우(스테이지에 도달했으나 아직 클리어 하지 못한경우)의 수를 구해준다.
-    let curr = stages.filter((x) => x === i).length;
-
-    // 3. i의 값과, 1,2의 값을 나눈값을 result에 담아준다.
-    result.push([i, curr / reach]);
-  }
-  console.log(result);
-  // 3. result의 값 중 나눈값을 기준으로 내림차순으로 정렬해준다.
-  result.sort((a, b) => b[1] - a[1]);
-
-  // 4. map을 통해 index값을 출력해준다.
-  return result.map((x) => x[0]);
-}
-
-function solution(N, stages) {
-  let result = [];
+  const answer = [];
 
   for (let i = 1; i <= N; i++) {
-    const reach = stages.filter((x) => x >= i).length;
-    const noClear = stages.filter((x) => x === i).length;
-    result.push([i, noClear / reach]);
+    const first = stages.filter((num) => num === i).length;
+    const second = stages.filter((num) => num >= i).length;
+
+    answer.push([first / second, i]);
   }
-  return result.sort((a, b) => b[1] - a[1]).map((item) => item[0]);
+
+  return answer
+    .sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]))
+    .map(([fail, idx]) => idx);
 }
-
-// 확률과 i를 []에 같이 넣어주고, 이를 확률을 기준으로 정렬 sort((a,b)=>b[1]-a[1])후 map을 통해 i를 item[0] 하는 법을 잘 이용해야겠다.
-
-function solution(N, stages) {
-  var answer = [];
-
-  for (let i = 1; i <= N; i++) {
-    const clear = stages.filter((f) => f >= i).length;
-    const noClear = stages.filter((f) => f === i).length;
-    answer.push([i, noClear / clear]);
-  }
-  console.log(answer.sort((a, b) => b[1] - a[1]).map((item) => item[0]));
-  return answer;
-}
-
-function solution(N, stages) {
-  let answer = [];
-  for (let i = 1; i <= N; i++) {
-    const clear = stages.filter((f) => f >= i).length;
-    const noClear = stages.filter((f) => f === i).length;
-
-    answer.push([i, noClear / clear]);
-  }
-  return answer.sort((a, b) => b[1] - a[1]).map((i) => i[0]);
-}
-
-solution(5, [2, 1, 2, 6, 2, 4, 3, 3]); // [3,4,2,1,5]
-
-solution(4, [4, 4, 4, 4, 4]); // [4,1,2,3]
