aa75b92b3f3bdfa726e688b1d4aadb4c91f18b08
rlaclghks123 <rlaclghks123@naver.com>
2024-01-06 11:32:58 +0900

    프로그래머스 Lv1 page4 2문제 복습
    1. 체육복
    2. 문자열 나누기

--- 프로그래머스Lv1/체육복.js
+++ 프로그래머스Lv1/체육복.js
@@ -1,53 +1,45 @@
-// 앞,뒤 학생에게만 체육복을 빌려줄 수 있음 Ex) 4는 3,5에게만
-// 체육복을 적절히 빌려 최대한 많은 학생이 수업을 듣도록
-// 체육수업을 들을 수 있는 학생의 최댓값 return
+// 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다
+// 최대한 많은 학생이 체육수업을 들어야 합니다.
+// 전체 학생의 수 n,
+// 체육복을 도난당한 학생들의 번호가 담긴 배열 lost,
+// 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve
+// 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+// 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다.  남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
 
-// 전체 학생수 n,
-// 도난당한 학생들 lost,
-// 여벌을 가진 학생 reserve
+// 1. 1부터 n까지 돌면서 도난을 확인합니다.
+// 2. 해당 학생이 도난 당했는지 확인
+// 3. 도난 당했다면
+// 3-1. 자신이 여분이 있는지 확인하여 여분이 있다면 count해주고, 여분에서 제거해줍니다.
+// 3-2. 이전 학생이 여분이 있는지 확인하여 여분이 있다면 count해주고, 여분에서 제거해줍니다.
+// 3-3. 다음 학생이 여분이 있는지 확인하여 여분이 있다면 count해주고, 여분에서 제거해줍니다.
+// 3-3-1. 단 다음 학생에게 빌릴 경우 다음 학생이 도난당하지 않은 경우만 가능
+// 4. 도난 당하지 않았다면 count 해줍니다.
+// 5. 총 체육복을 가지고 있는 학생을 출력합니다.
 
 function solution(n, lost, reserve) {
-  let count = 0;
-
+  let answer = 0;
   for (let i = 1; i <= n; i++) {
-    // 1. 잃어버렸는데 여분이 있을 경우
-    if (lost.includes(i) && reserve.includes(i)) {
-      count++;
-      reserve = reserve.filter((item) => item !== i);
-    }
+    if (lost.includes(i)) {
+      if (reserve.includes(i)) {
+        reserve = reserve.filter((item) => item !== i);
+        answer++;
+        continue;
+      }
 
-    // 2. 잃어버렸는데 이전학생이 여분이 있는경우
-    else if (lost.includes(i) && reserve.includes(i - 1) && !lost.includes(i - 1)) {
-      count++;
-      reserve = reserve.filter((item) => item !== i - 1);
-    }
+      if (reserve.includes(i - 1)) {
+        answer++;
+        reserve = reserve.filter((item) => item !== i - 1);
+        continue;
+      }
 
-    // 3. 잃어버렸는데 다음 학생이 여분이 있는경우
-    else if (lost.includes(i) && reserve.includes(i + 1) && !lost.includes(i + 1)) {
-      count++;
-      reserve = reserve.filter((item) => item !== i + 1);
-    }
-
-    //  4. 잃어버리지 않은 경우
-    else if (!lost.includes(i)) {
-      count++;
-    }
-    // 5. 그외 경우는 참여불가
-    else {
-      continue;
+      if (reserve.includes(i + 1) && !lost.includes(i + 1)) {
+        answer++;
+        reserve = reserve.filter((item) => item !== i + 1);
+        continue;
+      }
+    } else {
+      answer++;
     }
   }
-
-  return count;
+  return answer;
 }
-
-solution(5, [2, 4], [1, 3, 5]); // 5
-solution(5, [2, 4], [3]); // 4
-solution(3, [3], [1]); // 2
-solution(5, [5], [4]); // 5
-solution(5, [5], [1]); // 4
-solution(5, [4, 2], [3, 5]); //5
-solution(4, [2, 3], [3, 4]); //3
-
-// 다른사람코드  내것이 가장 이해하기 좋은듯
-// 단 else if 2, 3번째의 경우 !lost.includes(i - 1)), !lost.includes(i + 1))코드를 추가해야 한다.
