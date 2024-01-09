19c8d37d714c4ee9d4f063ea90460e4ee70a6d02
rlaclghks123 <rlaclghks123@naver.com>
2024-01-09 23:21:01 +0900

    프로그래머스 Lv2 Page-1 2문제
    1. 최대값과 최소값
    2. JadenCase 문자열 만들기

--- 프로그래머스Lv2/JadenCase문자열만들기.js
+++ 프로그래머스Lv2/JadenCase문자열만들기.js
@@ -1,31 +1,17 @@
-// JadenCase란 모든 단어의 첫 문자가 대문자, 나머지는 소문자인 문자열
-// 첫문자가 알파벳이 아닐땐 이어지는 알파벳은 소문자로
-// s를 JadenCase로 바꾼 문자열을 return
+// 1. 공백을 기준으로 나눈다.
+// 2. 공백을 기준으로 나눈 문자열 배열을 순회한다.
+// 3. 각 문자열에서 첫 글자가 숫자인지, 숫자가 아닌지 확인하여 새로운 문자열을 만든다.
+// 3-1. 첫 글자가 숫자라면 모든 글자를 소문자로 만든다.
+// 3-2. 첫 글자가 숫자가 아니라면 첫 글자를 대문자, 나머지는 소문자로 만든다.
+// 4. 3에서 만든 문자열을 가진 배열을 문자열로 바꿔 출력한다.
 
 function solution(s) {
-  var answer = '';
-  const split = s.split(' ');
+  const splited = s.split(' ');
 
-  // 1. 문자열을 ''으로 분리해준다.
-  // 2. 분리해준 문자열을 배열로 바꾼뒤, map으로 돌면서 첫글지인 경우(index===0)는 대문자로, 나머지는 소문자로 바꿔준 뒤, 문자열로 바꿔준다.
-  // 3. split으로 분리했기 때문에, 다시 문자열로 바꿔준다.
+  const newWords = splited.map((word) => {
+    if (Number.isInteger(word[0])) return word.toLowerCase();
+    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+  });
 
-  answer = split
-    .map((item) => [...item].map((m, i) => (i === 0 ? m.toUpperCase() : m.toLowerCase())).join(''))
-    .join(' ');
-
-  return answer;
-}
-
-solution('3people unFollowed me'); //	"3people Unfollowed Me"
-
-solution('for the last week'); //	"For The Last Week"
-
-// 다른사람의 코드
-
-function solution(s) {
-  return s
-    .split(' ')
-    .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
-    .join(' ');
+  return newWords.join(' ');
 }
--- 프로그래머스Lv2/최대값과최소값.js
+++ 프로그래머스Lv2/최대값과최소값.js
@@ -1,10 +1,12 @@
+// 1. s를 공백을 기준으로 나눈다.
+// 2. 나눈 값들중 Math.min를 통해 최소값을 구한다.
+// 3. 나눈 값들중 Math.max를 통해 최대값을 구한다.
+// 4. '최소값 최대값' 형태로 출력한다.
+
 function solution(s) {
-  var answer = '';
-  const splited = s.split(' ');
-  answer = Math.min(...splited) + ' ' + Math.max(...splited);
-  return answer;
+  const splited = [...s.split(' ')];
+  const min = Math.min(...splited);
+  const max = Math.max(...splited);
+
+  return `${min} ${max}`;
 }
-
-solution('-1 -2 -3 -4'); //	"-4 -1"
-
-solution('-1 -1'); // "-1 -1"
