663d267524dc0025dd52d24d7d9f0553b9cbb6ab
rlaclghks123 <rlaclghks123@naver.com>
2024-01-11 11:16:13 +0900

    프로그래머스 Lv1 page-4 2문제 복습
    1. 둘만의 암호
    2. 크레인 인형 뽑기

--- 프로그래머스Lv1/둘만의암호.js
+++ 프로그래머스Lv1/둘만의암호.js
@@ -1,28 +1,27 @@
-function solution(s, skip, index) {
-  const alpha = [];
+// 1. DB에 skip을 제외한 모든 소문자 영단어를 담아준다.
+// 2. s를 순회하면서 현재 단어에 index값을 더한 새로운 index값을 찾는다.
+// 2-1. 만약 z를 넘는다면 다시 a부터 시작
+// 3. 2에서 찾은 새로운 index값으로 DB에서 단어를 찾아 새로운 문자열을 만든다.
 
-  // 1. alpha 배열에 a~z 까지 모든 알파벳을 담아줍니다.
+function initDb(skip) {
+  const DB = [];
   for (let i = 97; i <= 122; i++) {
-    alpha.push(String.fromCharCode(i));
+    DB.push(String.fromCharCode(i));
   }
 
-  // 2. skip에 있는 알파벳은 alpha배열에서 제거해줍니다.
-  [...skip].forEach((word) => {
-    alpha.splice(alpha.indexOf(word), 1);
+  return DB.filter((char) => !skip.includes(char));
+}
+
+function solution(s, skip, index) {
+  const DB = initDb(skip);
+
+  const answer = [...s].map((word) => {
+    const beforeIdx = DB.indexOf(word);
+    let afterIdx = beforeIdx + index;
+
+    if (afterIdx >= DB.length) afterIdx = afterIdx % DB.length;
+    return DB[afterIdx];
   });
 
-  // 3.   s문자열을 배열로 만들어 하나씩 돌면서 각 요소의 alpha에서의 idx값을 찾아줍니다.
-  // 3-1. idx값에 문제에서 준 index값을 더해줍니다. (건너뛰는 기능)
-  // 3-2  만약 3-1에서 더한 값이 alpha의 배열을 넘어갈 경우 0부터 시작하도록 만들어줍니다.
-  // 3-3 map을 통해 3~-3-2 까지의 조건을 통해 새로운 배열을 만들어주고 문자열로 바꿔 출력합니다.
-  return [...s]
-    .map((word) => {
-      let idx = alpha.indexOf(word);
-      let sum = idx + index;
-      if (sum >= alpha.length) {
-        sum = sum % alpha.length;
-      }
-      return alpha[sum];
-    })
-    .join('');
+  return answer.join('');
 }
--- 프로그래머스Lv1/크레인인형뽑기.js
+++ 프로그래머스Lv1/크레인인형뽑기.js
@@ -1,44 +1,53 @@
+// 설명은 아래쪽에
+
 function solution(board, moves) {
-  let stack = [];
-  let count = 0;
+  let answer = 0;
+  const stack = [];
 
-  // 1.  moves와 board를 2중 for문으로 확인해준다.
-  for (let i = 0; i < moves.length; i++) {
-    // 2.moves의 값을 하나씩 확인한다. 단 배열이 0부터 시작하므로 -1을 해준다.
-    let pick = moves[i] - 1;
+  moves.forEach((position) => {
+    const curPosition = position - 1;
 
-    for (let j = 0; j < board.length; j++) {
-      // 3. 만약 아무것도 집은게 없다면(0) 넘겨준다.
-      if (board[j][pick] === 0) continue;
+    for (let i = 0; i < board.length; i++) {
+      const col = board[i];
 
-      // 4. 만약  이전에 집은것과 현재 집은것이 같은것이라면 stack에서 꺼낸뒤, count+=2 해준다.
-      if (stack[stack.length - 1] === board[j][pick]) {
-        stack.pop();
-        count += 2;
-      }
-      // 5. 현재 집은것이 이전에 집은것과 다르다면 stack에 담아준다.
+      if (col[curPosition] === 0) continue;
+      if (stack[stack.length - 1] !== col[curPosition]) stack.push(col[curPosition]);
       else {
-        stack.push(board[j][pick]);
+        stack.pop();
+        answer += 2;
       }
 
-      // 6. 집은것은 0으로 처리해주고 반복문을 멈춰준다.
-      board[j][pick] = 0;
+      board[i][curPosition] = 0;
       break;
     }
-  }
-  return count;
+  });
+  return answer;
 }
 
-solution(
-  [
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 3],
-    [0, 2, 5, 0, 1],
-    [4, 2, 4, 4, 2],
-    [3, 5, 1, 3, 1],
-  ],
-  [1, 5, 3, 5, 1, 2, 1, 4]
-  // 4 3 1 1 3 2 4
-); //	4
+// 1. moves를 돌면서 해당 위치에서 0이 아닌값을 찾습니다.
+// 2-1   1에서 찾은값과 stack의 가장 마지막값이 같은경우 2씩 cnt해주고 stack에서 삭제합니다.
+// 2-2   1에서 찾은값과 stack의 가장 마지막값이 다른경우 stack에 push합니다.
+// 2-3   1에서 찾은값을 0으로 바꿔줍니다.
 
-// 내 코드가 가장 이해하기 좋다.
+// [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 1, 0, 3],
+//   [0, 2, 5, 0, 1],
+//   [4, 2, 4, 4, 2],
+//   [3, 5, 1, 3, 1],
+// ];
+
+// 1번부터 moves : 1 5 3 5 1 2 1 4
+// 0번부터 moves : 0 4 2 4 0 1 0 3
+
+// [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 5, 0, 0],
+//   [0, 2, 4, 0, 2],
+//   [0, 5, 1, 3, 1],
+// ];
+
+// 4311324
+// 424
+// 11, 33 => 총 4개
