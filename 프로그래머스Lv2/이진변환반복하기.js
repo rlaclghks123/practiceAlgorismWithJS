d37784505f93f8f04b98bc9b30b1fe49bf2fba9b
rlaclghks123 <rlaclghks123@naver.com>
2024-01-12 11:02:32 +0900

    프로그래머스 Lv2 이진변환반복하기 복습

--- 프로그래머스Lv2/이진변환반복하기.js
+++ 프로그래머스Lv2/이진변환반복하기.js
@@ -1,59 +1,24 @@
-// x의 모든 0을 제거합니다.
-// x의 길이를 c라고 하면 x를 c를 2진법으로 표현한 문자열로 바꿉니다.
-// s가 1이 될때까지 반복해주고, 이진변환 횟수와, 총 제거된 0의 개수를 return
+// 1. s가 '1'이 될때까지 반복합니다.
+// 1-1. s의 길이를 기억해줍니다.
+// 1-2. s에서 0을 모두 제거해줍니다.
+// 1-3. (기억해둔 s의 길이 - 0을 제거한 s의 길이) 를 총 0의 개수(zeroCnt)에 더해줍니다.
+// 1-4. s를 (0을 제거한 s의 길이를 2진법으로 바꾼 문자열)로 교체합니다.
+// 1-5. 총 횟수를 +1 더해줍니다.
+// 2. [이진 변환의 횟수와, 변환 과정에서 제거된 모든 0의 개수]를 출력합니다.
 
 function solution(s) {
-  var answer = [];
-  let zeroCount = 0;
-  let count = 0;
-  // 1. (2,3,4)를  s가 '1'이 될떄까지 반복해준다.
-  // 2. count를 1번 세준다.
-  // 3. item이 0이면 zeroCount를 +1 해준뒤, 0을 지워준다.
-  // 4. 0을 지운 배열을 문자열로 바꾼뒤 총 길이를 2진법으로 바꿔준다.
+  let cnt = 0;
+  let zeroCnt = 0;
 
   while (s !== '1') {
-    count++;
-    s = [...s]
-      .map((item) => {
-        if (item === '0') {
-          zeroCount++;
-          return;
-        }
-        return item;
-      })
-      .join('')
-      .length.toString(2);
-  }
-  return [count, zeroCount];
-}
+    const beforeS = s;
+    s = s.replaceAll('0', '');
+    const afterS = s;
 
-solution('110010101001'); // [3,8]
-solution('01110'); // [3,3]
-solution('1111111'); // [4,1]
-
-// 다른 사람의 코드
-// 정규 표현식을 사용해서 0을 바꿔줬다.
-
-function solution(s) {
-  var answer = [0, 0];
-  let zeroCount = 0;
-  let count = 0;
-
-  while (s !== '1') {
-    count++;
-
-    s = [...s]
-      .map((item) => {
-        if (item === '0') {
-          zeroCount++;
-          return;
-        }
-        return item;
-      })
-      .join('').length;
-
-    s = s.toString(2);
+    zeroCnt += beforeS.length - afterS.length;
+    s = s.length.toString(2);
+    cnt += 1;
   }
 
-  return [count, zeroCount];
+  return [cnt, zeroCnt];
 }
