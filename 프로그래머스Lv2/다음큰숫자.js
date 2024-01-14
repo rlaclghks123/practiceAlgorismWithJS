1715e691077cd31fa1a2333396b13877838769fa
rlaclghks123 <rlaclghks123@naver.com>
2024-01-14 11:26:20 +0900

    프로그래머스 Lv2 - 다음 큰 숫자 복습

--- 프로그래머스Lv2/다음큰숫자.js
+++ 프로그래머스Lv2/다음큰숫자.js
@@ -1,45 +1,17 @@
-// 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
-// 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
-// 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
-// 조건을 만족하는 다음 큰 숫자를  return
+// 1. n을 2진법으로 바꿨을 때 1의 개수를 찾습니다.
+// 2. 반복문을 통해 n부터 +1 계속 더해 다음값을 찾습니다.
+// 2-1. 만약 다음값을 2진법으로 바꿨을 때 1의 개수가 같다면 다음값을 출력합니다.
+// 2-2. 만약 다음값을 2진법으로 바꿨을 때 1의 개수가 다르다면 계속 n에 +1씩 더해 반복해줍니다.
 
 function solution(n) {
-  var answer = 0;
+  let start = getNumberOfOneLength(n);
 
-  // 1. 현재 값의 2진수로 변환하여 0을 제거한 길이를 구한다.
-  let nLen = n.toString(2).replaceAll(0, '').length;
-
-  let nextLen = -1;
-  let nextNum = n + 1;
-
-  // 2. 현재수 +1씩 하여 2진수로 변환하여 0을 제거한 길이가 같은경우 번호를 찾는다.
-  while (nLen !== nextLen) {
-    nextLen = nextNum.toString(2).replaceAll(0, '').length;
-    if (nLen === nextLen) break;
-    nextNum++;
+  while (true) {
+    let target = getNumberOfOneLength(++n);
+    if (start === target) return n;
   }
-
-  return nextNum;
 }
 
-solution(78); // 83
-solution(15); // 23
-
-// 다른사람의 풀이
-// 정규표현식 사용 및 재귀로 풀었다.
-
-function solution(n, a = n + 1) {
-  return n.toString(2).match(/1/g).length == a.toString(2).match(/1/g).length
-    ? a
-    : solution(n, a + 1);
-}
-function solution(n) {
-  var answer = 0;
-  let nextLen = n.toString(2).replaceAll('0', '').length;
-  let nextNum = n + 1;
-
-  while (nextLen !== nextNum.toString(2).replaceAll('0', '').length) {
-    nextNum++;
-  }
-  return nextNum;
+function getNumberOfOneLength(n) {
+  return n.toString(2).replaceAll('0', '').length;
 }
