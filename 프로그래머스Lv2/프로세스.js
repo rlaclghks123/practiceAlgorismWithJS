f0b01d7c45074ec20a111f8cc2de3bf2642bd2e9
rlaclghks123 <rlaclghks123@naver.com>
2024-02-04 19:17:40 +0900

    프로그래머스 Lv2 프로세스 복습

--- 프로그래머스Lv2/프로세스.js
+++ 프로그래머스Lv2/프로세스.js
@@ -1,34 +1,31 @@
-// 1. priorities를 index와 함께 mapping 한다.  //setIndex
+// 1. 우선순위 배열을 [우선순위, 실행되는 위치] 형태로 새롭게 만들어줍니다.
 
-// 2. location이 몇번째 실행되는지 확인  // countWhenExecuteLocation
-// 2-1 while문으로 Arr의 값이 존재하면 계속 반복해줍니다.
-// 2-1-1 첫번째 값을 꺼내 자신보다 우선순위가 높은 친구가 있는지 확인합니다.
-// 2-1-2 있다면 arr에 꺼낸값을 다시 push합니다.
-// 2-1-3 없다면 count++ 해주고, location과 index값이 같은지 비교해 같은경우 count값을 return
+// 2. 1에서 만든 새로운 배열의 길이가 없을때까지 반복해줍니다.
+// 2-1. shift를 이용해 첫번째 값을 뽑아줍니다.
+// 2-2. 2-1에서 뽑은 값보다 우선순위가 높은것이 있는지 찾습니다.
 
-// 3. 2에서 찾아낸 count값을 출력합니다.
+// 2-2-1. 우선순위가 높은게 있다면 다시 1에서 만든 배열에 push해줍니다.
+// 2-2-2. 우선순위가 높은게 없다면
+
+// 2-2-2-1. 현재 location과 찾는 location을 비교하여 같다면 cnt를 출력합니다.
+// 2-2-2-2. 현재 location과 찾는 location을 비교하여 다르다면 cnt를 +1 해준뒤 다시 반복해줍니다.
 
 function solution(priorities, location) {
-  let prioritiesWithIndex = setIndex(priorities);
-  return countWhenExecuteLocation(prioritiesWithIndex, location);
-}
+  let cnt = 1;
+  const prioritiesWithLocation = priorities.map((item, idx) => [item, idx]);
 
-function setIndex(arr) {
-  return arr.map((item, index) => [item, index]);
-}
+  while (prioritiesWithLocation.length) {
+    const shifted = prioritiesWithLocation.shift();
+    const [curPriorities, curLocation] = shifted;
+    const isHigherPriority = prioritiesWithLocation.find(
+      ([priorities, idx]) => curPriorities < priorities
+    );
 
-function countWhenExecuteLocation(arr, location) {
-  let count = 0;
-
-  while (arr.length) {
-    let shifted = arr.shift();
-    const isPriority = arr.some((item) => item[0] > shifted[0]);
-
-    if (isPriority) {
-      arr.push(shifted);
-      continue;
+    if (isHigherPriority) {
+      prioritiesWithLocation.push(shifted);
+    } else {
+      if (curLocation === location) return cnt;
+      cnt++;
     }
-    count++;
-    if (shifted[1] === location) return count;
   }
 }
