d9054f215912f75cca08d61cfa56288714ad16b0
rlaclghks123 <rlaclghks123@naver.com>
2024-02-05 11:26:23 +0900

    프로그래머스 Lv2 피로도 복습

--- 프로그래머스Lv2/못푼문제들/피로도.js
+++ 프로그래머스Lv2/못푼문제들/피로도.js
@@ -1,49 +1,30 @@
-// 최소 피로도는 탐험을 위한 최소한 피로도
-// 소모 피로도는 탐험후 소모되는 피로도
-// 유저가 탐험할 수 있는 최대 던전 수를 return
-// [최소피로도, 소모피로도]
-// 소모피로도<=최소피로도    && 1이상 1,000이하
-// 서로 다른 던전의 []가 서로 같을 수 있습니다.
+// 1. 방문처리를 위해 visit 배열을 만들어줍니다.
+
+// 2. dungeons를 깊이우선으로 완전탐색을 시작합니다.
+// 2-1. cnt, max를 비교하여 최대값을 max에 저장해줍니다.
+// 2-2. dungeons를 순회하면서 최소피로도가 k이하면서 방문한적 없는 경우
+// 2-2-1. 방문처리를 해줍니다.
+// 2-2-2. 카운트+1, k에서 소비피로도를 소비한 뒤, 다시 exploreDungeons를 방문합니다.(dfs)
+// 2-2-3. dfs를 빠져나오면 방문처리를 다시 false를 통해 다른 경우의 dfs에 문제 없도록 해줍니다.
+
+// 3. 최대값을 출력해줍니다.
 
 function solution(k, dungeons) {
-  var answer = [];
+  let max = 0;
+  const visit = Array.from({ length: dungeons.length }, () => false);
 
-  // 방문체크를 할 visited 배열을 false로 초기화해준다.
-  const visited = Array.from({ length: dungeons.length }, () => false);
+  exploreDungeons(0, k);
+  return max;
 
-  // dfs함수를 통해 모든던전을 탐험해본다.
-  function dfs(count, k) {
-    //count값을 담아준다.
-    answer.push(count);
+  function exploreDungeons(cnt, k) {
+    max = Math.max(max, cnt);
 
-    // 모든 던전을 돌면서 현재 피로도 이하 && 방문한 적 없는 던전을 방문한다.
-    dungeons.forEach((item, i) => {
-      const [minimum, used] = item;
-      if (minimum <= k && !visited[i]) {
-        // 방문했기 때문에 방문처리 해준다.
-        visited[i] = true;
-        // 현재 위치에서 dfs를 통해 탐험해준다.
-        dfs(count + 1, k - used);
-        // 한번 최대한 깊숙히 들어갔다 왔고, 다시 새출발 해야하기 때문에 방문처리를 false로 바꿔준다.
-        visited[i] = false;
+    dungeons.forEach(([minPirodo, consumePirodo], idx) => {
+      if (minPirodo <= k && !visit[idx]) {
+        visit[idx] = true;
+        exploreDungeons(cnt + 1, k - consumePirodo);
+        visit[idx] = false;
       }
     });
   }
-  dfs(0, k);
-  return Math.max(...answer);
 }
-
-solution(80, [
-  [80, 20],
-  [50, 40],
-  [30, 10],
-]); // 3
-
-solution(40, [
-  [40, 20],
-  [10, 10],
-  [10, 10],
-  [10, 10],
-  [10, 10],
-]);
-//  4
